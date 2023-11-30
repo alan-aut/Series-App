cd3134aef0a6d3401a88d82ff4237295288855d3
alan-aut <aut43ase@gmail.com>
2023-11-29 22:48:06 -0600

    Inicio de Proyecto

---
+++ src/app/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD1paIPbvj5mvpf2i4lbeOY1GOrWwUkiq0",
+    authDomain: "radiofrecuencias-app.firebaseapp.com",
+    projectId: "radiofrecuencias-app",
+    storageBucket: "radiofrecuencias-app.appspot.com",
+    messagingSenderId: "806319986820",
+    appId: "1:806319986820:web:a8c3e8dc6e2506a0cf8ad1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
